09639151ea25efd750528c1a33b325bf07d5254e
slipStas <41761920+slipStas@users.noreply.github.com>
2020-05-07 22:11:55 +0300

    create app

---
+++ Millionaire/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  Millionaire
+//
+//  Created by Stanislav Slipchenko on 07.05.2020.
+//  Copyright © 2020 Stanislav Slipchenko. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
